9903deb6a75a6777abc565026a2b5234f1a786dd
Camila Silva <silva.camilam27@gmail.com>
2022-03-31 23:28:51 -0300

    first commit, homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './template/header-component/header-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { ContentComponentComponent } from './template/content-component/content-component.component';
+import {MatCardModule} from '@angular/material/card';
+import { FooterComponentComponent } from './template/footer-component/footer-component.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    ContentComponentComponent,
+    FooterComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
